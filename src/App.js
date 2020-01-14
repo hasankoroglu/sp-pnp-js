0d493c7349eb96c88e95057bc9397b5451a604e4
hasankoroglu <hasan@hasankoroglu.com>
2020-01-14 18:13:45 +0300

    finito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
-//import syntaxHighlight from "./syntaxHighlight"
-import { loadPageContext } from 'sp-rest-proxy/dist/utils/env';
-//import { sp } from "@pnp/sp/presets/all";
+import React from "react";
+import "./App.css";
+import syntaxHighlight from "./syntaxHighlight";
+import { loadPageContext } from "sp-rest-proxy/dist/utils/env";
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/folders";
+import "@pnp/sp/files";
 
 export default function App() {
-  loadPageContext();
+  loadPageContext()
+    .then(async _spPageContextInfo => {
+      let items = await sp.web
+        .getFolderByServerRelativeUrl(`${_spPageContextInfo.webServerRelativeUrl}/SitePages`)
+        .files();
+      let result = JSON.stringify(items, null, 1);
+      document.getElementById("result").innerHTML = syntaxHighlight(result);
+    })
+    .catch(console.warn);
 
   return (
     <div>
